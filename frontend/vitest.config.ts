b46ace382a24f50ef8e51a3a8c8a907c548611be
Catalina DANILA <catalinadanila6@gmail.com>
2026-03-30 12:48:11 +0200

    fix ci ficixxixiixix

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineVitestConfig({
+export default defineConfig({
   test: {
     environment: 'node',
+    include: ['app/**/*.test.ts'],
   },
 })
