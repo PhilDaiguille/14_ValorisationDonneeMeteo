0ff49b8b4b95be2fa92b2686c1edaa1575181865
cgirard <camille.girard1995@gmail.com>
2026-04-18 11:54:01 +0200

    add docker hardene image

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,6 @@
 set -e
 
 echo "Applying database migrations..."
-uv run python manage.py migrate --noinput
-
-
-echo "Collecting static files..."
-uv run python manage.py collectstatic --noinput
+python manage.py migrate --noinput
 
 exec "$@"
